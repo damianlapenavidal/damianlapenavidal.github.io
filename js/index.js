69c3a9091d3f20c23edc8ff3c35e743c8cccc3fc
damianlapenavidal <damian.lapenavidal@gmail.com>
2025-07-22 21:39:51 +0200

    Added in progress picture + Modified social media symbols

--- js/index.js
+++ js/index.js
@@ -1,9 +1,18 @@
-function validarFormulario() {
-    const nombre = document.getElementById("nombre").value;
-    if (nombre === "") {
-        alert("Por favor, introduce tu nombre.");
-        return false; // Evita el envío del formulario
-    }
-    alert("Formulario enviado con éxito. ¡Gracias!");
-    return true;
-}
+document.addEventListener("DOMContentLoaded", function () {
+  const connectLinks = document.querySelectorAll('a[href="#Follow"]');
+  const followContent = document.querySelector(".follow-content");
+
+  connectLinks.forEach(link => {
+    link.addEventListener("click", function () {
+      // Delay to allow scroll to finish
+      setTimeout(() => {
+        followContent.classList.add("glow-effect");
+
+        // Remove class after animation completes
+        setTimeout(() => {
+          followContent.classList.remove("glow-effect");
+        }, 2000);
+      }, 400);
+    });
+  });
+});
